c474996d21b993d107e2bc5aca094df4defa2c8e
SamBritt <sam.britt2@yahoo.com>
2019-03-11 10:26:41 -0500

    Completed Fetching other people's data exercise.

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,12 +1,46 @@
+//Displaying Foods
+const foodList = document.querySelector('.foodList');
+const foodListDivs = foodList.querySelectorAll('div');
+
+//converts any array to a string representing DOM input Elements.
+const convertToString = (arr) => {
+    let constructMe = '<div>';
+    for(let i in arr){
+       constructMe += `<p>${i}: ${arr[i]}</p>`;
+    }
+    constructMe += '</div>'
+    return constructMe;
+}
+
+//Takes return value from convertToString and appends to DOM
+const insertToDOM = (element) => {
+    foodList.innerHTML += element;
+}
+
+// fetch("http://localhost:8088/foods")
+//     .then(foods => foods.json())
+//     .then(parsedFoods => {
+//         parsedFoods.forEach(element => {
+//             const foodAsHTMl = convertToString(element);
+//             // console.log(foodAsHTMl);
+//             insertToDOM(foodAsHTMl);
+//         });
+//     })
+//Fetching Other People's Data
+
+//Fetch data from our API
 fetch("http://localhost:8088/foods")
     .then(foods => foods.json())
     .then(parsedFoods => {
-        console.table(parsedFoods)
+        parsedFoods.forEach(element => {
+//Fetch data from OpenFood API
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${element.barcode}.json`)
+            .then(response => response.json())
+            .then(productInfo => {
+                //Creates ingredient key/value from fetched ingredient value
+                element.ingredients = productInfo.product.ingredients_text_en;
+                const foodAsHTMl = convertToString(element);
+                insertToDOM(foodAsHTMl);
+            })
+        });
     })
-
-const classList = document.querySelector('.classList');
-
-const convertToString = () => {
-    let structString = '';
-    return structString;
-}
